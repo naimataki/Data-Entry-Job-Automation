dcd2aba27dcc53619ee9bb2b3ce367e2c19eca09
Naima Takiouti <106370938+naimataki@users.noreply.github.com>
2026-06-13 11:37:08 +0100

    final

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 url = "https://appbrewery.github.io/Zillow-Clone/"
 form_url = "https://docs.google.com/forms/d/e/1FAIpQLScsESx6SNsbCbpV2MzYbpBzv3Wc6t6ePTAit9QRZK1GAwg2nw/viewform?usp=header"
+responses_url = "https://docs.google.com/forms/d/1Xx7MahJ_TLhVVDsBLVoUXRaFSv6IB2llx-RdwEiir2A/edit#responses"
 
 header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
@@ -40,11 +42,24 @@ addresses = [address.get_text().replace(" | ", " ").strip() for address in addre
 chrome_options = webdriver.ChromeOptions()
 # Keep chrome browser open after program finishes
 chrome_options.add_experimental_option("detach", True)
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get(form_url)
-time.sleep(2)
+for i in range(2):
+    driver.get(form_url)
+    time.sleep(2)
+    inputs = driver.find_elements(By.CLASS_NAME, "whsOnd")
+    inputs[0].send_keys(addresses[i])
+    inputs[1].send_keys(prices[i])
+    inputs[2].send_keys(links[i])
+    driver.find_element(By.CLASS_NAME, "NPEfkd").click()
+    time.sleep(2)
 
-inputs = driver.find_elements(By.CLASS_NAME, "whsOnd")
+driver.get(responses_url)
+time.sleep(2)
+driver.find_element(By.CLASS_NAME, "NPEfkd").click()
+
+
 
